14b5a4ac9585d5d8519221ddcd853e2ab8541763
Bach Bui <bui.duybach@gmail.com>
2022-03-16 16:30:46 -0700

    add gridview to result but comment it out for now

--- cyc-next-app/lib/components/richoutput-panel/result-panel/ResultView.tsx
+++ cyc-next-app/lib/components/richoutput-panel/result-panel/ResultView.tsx
@@ -10,6 +10,7 @@ import { useSelector } from "react-redux";
 import { ICodeLine } from "../../../interfaces/ICodeEditor";
 import store from "../../../../redux/store";
 import { ContentType, SubContentType } from "../../../interfaces/IApp";
+import GridLayout from "react-grid-layout";
 
 const ResultWithNoSSR = dynamic(() => import("react-plotly.js"), {
     ssr: false,
@@ -47,7 +48,10 @@ const ResultView = (props: any) => {
         // setContainerMounted(true);
     });
 
+    const rowHeight = 50; //unit: px
+    const screenSize = 2000; //unit: px;
     const resultViewId = "StyledResultViewID";
+    const cols = 10; //unit: grid
     const renderResult = () => {
         const state = store.getState();
         const inViewID = state.projectManager.inViewID;
@@ -59,58 +63,76 @@ const ResultView = (props: any) => {
             console.log("codeWithResult", codeWithResult);
             return (
                 <StyledResultView id={resultViewId}>
-                    {codeWithResult.length > 0
-                        ? codeWithResult.map((codeResult: ICodeLine) => (
-                            //   <ScrollIntoViewIfNeeded
-                            //       active={codeResult.lineID == activeLine}
-                            //       options={{
-                            //           block: "start",
-                            //           inline: "center",
-                            //           behavior: "smooth",
-                            //           boundary:
-                            //               document.getElementById(resultViewId),
-                            //       }}
-                            //   >
-                                  <SingleResult
-                                      key={codeResult.lineID}
-                                      variant="outlined"
-                                      focused={codeResult.lineID == activeLine}
-                                  >
-                                      {codeResult?.result?.subType ===
-                                          SubContentType.PLOTLY_FIG &&
-                                          React.createElement(
-                                              ResultWithNoSSR,
-                                              setLayout(
+                    {/* <GridLayout
+                        // ref={gridRef}
+                        measureBeforeMount={true}
+                        className="layout"
+                        rowHeight={rowHeight}
+                        width={screenSize}
+                        cols={cols}
+                        margin={[5, 5]}
+                        isResizable={true}
+                    > */}
+                        {codeWithResult.length > 0
+                            ? codeWithResult.map((codeResult: ICodeLine) => (
+                                //   <ScrollIntoViewIfNeeded
+                                //       active={codeResult.lineID == activeLine}
+                                //       options={{
+                                //           block: "start",
+                                //           inline: "center",
+                                //           behavior: "smooth",
+                                //           boundary:
+                                //               document.getElementById(
+                                //                   resultViewId
+                                //               ),
+                                //       }}
+                                //   >
+                                      <SingleResult
+                                          key={codeResult.lineID}
+                                          variant="outlined"
+                                          focused={
+                                              codeResult.lineID == activeLine
+                                          }
+                                      >
+                                          {codeResult?.result?.subType ===
+                                              SubContentType.PLOTLY_FIG &&
+                                              React.createElement(
+                                                  ResultWithNoSSR,
+                                                  setLayout(
+                                                      codeResult?.result
+                                                          ?.content
+                                                  )
+                                              )}
+                                          {codeResult?.result?.subType ===
+                                              SubContentType.APPLICATION_JSON &&
+                                              JSON.stringify(
                                                   codeResult?.result?.content
-                                              )
+                                              )}
+                                          {codeResult?.result?.subType.includes(
+                                              "image"
+                                          ) && (
+                                              <img
+                                                  src={
+                                                      "data:" +
+                                                      codeResult?.result
+                                                          ?.subType +
+                                                      ";base64," +
+                                                      codeResult?.result
+                                                          ?.content
+                                                  }
+                                              />
                                           )}
-                                      {codeResult?.result?.subType ===
-                                          SubContentType.APPLICATION_JSON &&
-                                          JSON.stringify(
-                                              codeResult?.result?.content
-                                          )}
-                                      {codeResult?.result?.subType.includes(
-                                          "image"
-                                      ) && (
-                                          <img
-                                              src={
-                                                  "data:" +
-                                                  codeResult?.result?.subType +
-                                                  ";base64," +
+                                          {/* Display video/ audio */}
+                                          {codeResult?.result?.subType ===
+                                              SubContentType.TEXT_HTML &&
+                                              ReactHtmlParser(
                                                   codeResult?.result?.content
-                                              }
-                                          />
-                                      )}
-                                      {/* Display video/ audio */}
-                                      {codeResult?.result?.subType ===
-                                          SubContentType.TEXT_HTML &&
-                                          ReactHtmlParser(
-                                              codeResult?.result?.content
-                                          )}
-                                  </SingleResult>
-                            //   </ScrollIntoViewIfNeeded>
-                          ))
-                        : null}
+                                              )}
+                                      </SingleResult>
+                                  // </ScrollIntoViewIfNeeded> 
+                              ))
+                            : null}
+                    {/* </GridLayout> */}
                 </StyledResultView>
             );
         } else return null;
