2f465ff9161c398af9ca07873b0d7283fe13b479
HoaTV <thaivanhoa.dtmtptit@gmail.com>
2022-03-16 16:04:34 +0700

    sync test

--- cyc-next-app/cypress/integration/cyc/cyc.spec.js
+++ cyc-next-app/cypress/integration/cyc/cyc.spec.js
@@ -1,29 +1,46 @@
-import { codeTestOutput, codeTestDF, codeTestEditor } from '../data/code-text';
-const WAIT_TIME_OUT = 2000;
-describe('Check UI of all Element on Cyc web app', () => {
+import {
+    codeTestOutput,
+    codeTestDF,
+    codeTestEditor,
+    codeTestEditorState,
+    codeTestPlotinAudioVideo,
+    codeTestMatplotBar,
+    codeTestMatplotBarPhase1,
+    codeTestMatplotBarPhase2,
+} from '../data/code-text';
+const WAIT_TIME_OUT = 1000;
+describe('Check Code Editor', () => {
     it('Check state of Editor', () => {
-        cy.visit('/').wait(WAIT_TIME_OUT);
+        cy.visit('/').wait(3000);
         let lines = cy
             .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+            .as('editor')
             .children('.cm-line');
 
         // make sure codeEditor have text from old state
         expect(lines).to.exist;
         lines.its('length').should('be.gt', 0);
+        cy.wait(WAIT_TIME_OUT);
     });
+});
 
+describe('Check Console', () => {
     it('Check print console', () => {
         let editor = cy
             .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
             .as('editor');
         // make sure have text on output
+        editor.focus();
         removeText(editor);
         editor.type(codeTestOutput);
         editor.type('{ctrl}l');
 
         cy.get('#CodeOutputContent > :nth-child(1)').contains('test');
+        cy.wait(WAIT_TIME_OUT);
     });
+});
 
+describe('Check Comletion of Editor', () => {
     it('Check autocompletion', () => {
         let editor = cy
             .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
@@ -41,13 +58,17 @@ describe('Check UI of all Element on Cyc web app', () => {
         cy.get('.cm-read-more-btn').click();
         cy.get('#code-doc-content').should('not.exist');
 
+        // check data
+        let content = cy
+            .get('[data-cy="code-editor"] > .cm-editor > .cm-tooltip-autocomplete')
+            .as('autocomplete-content');
+        content.contains('drop_duplicates');
+
         // make sure keyboard still work
         editor = cy.get('@editor');
         editor.type('{backspace}');
         editor.type('{esc}');
         cy.get('.cm-tooltip-autocomplete').should('not.exist');
-
-        // TODO: check data
 
         // make sure have signature tooltip
         editor = cy.get('@editor');
@@ -59,11 +80,6 @@ describe('Check UI of all Element on Cyc web app', () => {
         editor.type('{esc}');
         cy.get('.cm-tooltip-signature').should('not.exist');
 
-        // TODO: make hover work
-        // make sure tooltip still work
-        // cy.get('.cm-activeLine > :nth-child(1)').should('be.visible');
-        // cy.get('.cm-activeLine > :nth-child(1)').click();
-
         editor = cy.get('@editor');
         removeText(editor);
         editor.type(codeTestDF);
@@ -71,8 +87,11 @@ describe('Check UI of all Element on Cyc web app', () => {
         editor.type('{enter}');
         editor.type('df.drop("');
         cy.get('.cm-tooltip-autocomplete').should('be.visible');
+        cy.wait(WAIT_TIME_OUT);
     });
+});
 
+describe('Check DataFrame', () => {
     it('Check dataframe', () => {
         let editor = cy
             .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
@@ -87,6 +106,157 @@ describe('Check UI of all Element on Cyc web app', () => {
 
         cy.get('*[class^="StyledComponents__StyledTableViewHeader"] > :nth-child(2)').click();
         cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(1)').contains('Id');
+        cy.wait(WAIT_TIME_OUT);
+    });
+});
+
+describe('Check DF Autocompletion', () => {
+    it('Check state of Editor', () => {
+        let lines = cy
+            .get('.cm-theme-light > .cm-editor > .cm-scroller > .cm-content')
+            .as('df-editor')
+            .children('.cm-line');
+        expect(lines).to.exist;
+        lines.its('length').should('be.gt', 0);
+
+        let dfEditor = cy.get('@df-editor');
+        dfEditor.focus();
+        dfEditor.type('("');
+
+        cy.get('.cm-tooltip-autocomplete').should('be.visible');
+        cy.get('.cm-completionLabel').contains('Alley');
+        cy.wait(WAIT_TIME_OUT);
+    });
+});
+
+describe('Check data in Redux store', () => {
+    it('has expected CodeEditor state when typing', () => {
+        cy.visit('/').wait(10000);
+        let editor = cy
+            .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+            .as('editor');
+        editor.focus();
+        removeText(editor);
+        editor.type(codeTestEditorState);
+
+        cy.wait(5000);
+        // check file save state | saved or not
+        cy.window()
+            .its('store')
+            .invoke('getState')
+            .its('codeEditor.fileSaved')
+            .should('equal', true);
+
+        // check codeText
+        cy.window()
+            .its('store')
+            .invoke('getState')
+            .its('codeEditor')
+            .its('codeText')
+            .then((result) => {
+                for (let key in result) {
+                    cy.log(key);
+                    assert.deepEqual(result[key], [`print('test1')`, `print('test2')`]);
+                }
+            });
+
+        // check length code line
+        cy.window()
+            .its('store')
+            .invoke('getState')
+            .its('codeEditor')
+            .its('codeLines')
+            .then((result) => {
+                for (let key in result) {
+                    cy.log(key);
+                    assert.equal(result[key].length, 2);
+                }
+            });
+        cy.wait(WAIT_TIME_OUT);
+    });
+
+    it('has expected ProjectManager state', () => {
+        cy.window()
+            .its('store')
+            .invoke('getState')
+            .its('projectManager')
+            .its('openFiles')
+            .then((files) => {
+                let i = 0;
+                for (let key in files) {
+                    i++;
+                }
+                // check have 3 files
+                assert.equal(i, 3);
+            });
+        cy.wait(WAIT_TIME_OUT);
+    });
+
+    it('has expected DataFrames state', () => {
+        let editor = cy
+            .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+            .as('editor');
+        editor.focus();
+        removeText(editor);
+        editor.type(codeTestDF);
+        editor.type('{ctrl}l');
+        cy.wait(3000);
+        cy.window()
+            .its('store')
+            .invoke('getState')
+            .its('dataFrames')
+            .its('metadata')
+            .its('cdf')
+            .its('columns')
+            .then((columns) => {
+                let i = 0;
+                for (let key in columns) {
+                    i++;
+                }
+                // check have 20 colums
+                assert.equal(i, 20);
+            });
+        cy.wait(WAIT_TIME_OUT);
+    });
+});
+
+describe('check Ploting', () => {
+    it('still render Ploting result', () => {
+        let editor = cy
+            .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+            .as('editor');
+        editor.focus();
+        removeText(editor);
+        cy.wait(1000);
+        editor = cy.get('@editor');
+        editor.type(codeTestMatplotBarPhase1);
+        cy.wait(10000);
+        editor = cy.get('@editor');
+        editor.type(codeTestMatplotBarPhase2);
+        editor = cy.get('@editor');
+        editor.type('{selectall}');
+        editor.type('{ctrl}k');
+        editor.type('{ctrl}l');
+        cy.wait(1000);
+        cy.get('.MuiPaper-root > img').should('be.visible');
+        cy.wait(WAIT_TIME_OUT);
+    });
+
+    it('still render Audio and Video', () => {
+        let editor = cy
+            .get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+            .as('editor');
+        editor.focus();
+        removeText(editor);
+        cy.wait(1000);
+        editor = cy.get('@editor');
+        editor.type(codeTestPlotinAudioVideo);
+        cy.wait(1000);
+        editor = cy.get('@editor');
+        editor.type('{selectall}');
+        editor.type('{ctrl}k');
+        editor.type('{ctrl}l');
+        cy.wait(WAIT_TIME_OUT);
     });
 });
 
--- cyc-next-app/cypress/integration/data/code-text.js
+++ cyc-next-app/cypress/integration/data/code-text.js
@@ -1,5 +1,8 @@
 export const codeTestOutput = `print('test')`;
 
+export const codeTestEditorState = `print('test1')
+print('test2')`;
+
 export const codeTestDF = `import pandas as pd
 cdf = cd.DataFrame(pd.read_csv('data/housing_data/data.csv'))`;
 
@@ -7,3 +10,24 @@ export const codeTestEditor = `import pandas as pd
 cdf = cd.DataFrame(pd.read_csv('data/housing_data/data.csv'))
 df = pd.DataFrame()
 `;
+
+export const codeTestMatplotBarPhase1 = `import matplotlib.pyplot as plt
+labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+men_means = [20, 35, 30, 35, 27]
+women_means = [25, 32, 34, 20, 25]
+men_std = [2, 3, 4, 1, 2]
+women_std = [3, 5, 2, 3, 3]
+width = 0.35`;
+export const codeTestMatplotBarPhase2 = `
+fig, ax = plt.subplots()
+ax.bar(labels, men_means, width, yerr=men_std, label='Men')
+ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,label='Women')
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+ax.legend()
+plt.show()`;
+
+export const codeTestPlotinAudioVideo = `from IPython.display import Audio
+Audio('https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3', autoplay=True)
+from IPython.display import Video
+Video('https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4')`;
