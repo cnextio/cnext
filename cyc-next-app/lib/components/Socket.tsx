84ec74ac18d8423300d9d7d162b086d4f53f580f
HoaTV <thaivanhoa.dtmtptit@gmail.com>
2022-03-29 00:49:16 +0700

    add network

--- cyc-next-app/lib/components/Socket.tsx
+++ cyc-next-app/lib/components/Socket.tsx
@@ -1,5 +1,5 @@
-import openSocket from "socket.io-client";
-// export const CODE_SERVER_SOCKET_ENDPOINT = "http://localhost:4000";
-export const CODE_SERVER_SOCKET_ENDPOINT = 'http://server:80';
+import openSocket from 'socket.io-client';
+export const CODE_SERVER_SOCKET_ENDPOINT = 'http://localhost:4000';
+// export const CODE_SERVER_SOCKET_ENDPOINT = 'http://server:80';
 const socket = openSocket(CODE_SERVER_SOCKET_ENDPOINT);
 export default socket;
