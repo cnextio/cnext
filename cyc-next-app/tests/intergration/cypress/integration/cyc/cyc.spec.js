55f5f4c264b9046ee5e4a8ba2a9289bca8e7bb35
Vick Nguyen <vicknguyen1992@gmail.com>
2022-04-07 19:52:49 +0700

    update cypress

--- cyc-next-app/tests/intergration/cypress/integration/cyc/cyc.spec.js
+++ cyc-next-app/tests/intergration/cypress/integration/cyc/cyc.spec.js
@@ -28,156 +28,156 @@ const randomString = () => {
     return text;
 }
 
-// describe('Test Code Editor', () => {
+describe('Test Code Editor', () => {
 
-//     before(() => {
-//         cy.visit('/');
-//         cy.get('[toolbarname="main.py"]').trigger("click");
-//     })
+    before(() => {
+        cy.visit('/');
+        cy.get('[toolbarname="main.py"]').trigger("click");
+    })
 
-//     beforeEach(() => {
-//         cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content').as('editor');
-//         cy.get('@editor').focus();
-//         removeText(cy.get('@editor'));
-//     })
+    beforeEach(() => {
+        cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content').as('editor');
+        cy.get('@editor').focus();
+        removeText(cy.get('@editor'));
+    })
 
-//     it('Check print console', () => {
-//         cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
-//           .as('editor');
-//         cy.get('@editor').focus();
-//         cy.get('@editor').type(codeCheckConsole);
-//         if (isMacOSPlatform()) {
-//             cy.get('@editor').type('{command}l');
-//         } else {
-//             cy.get('@editor').type('{ctrl}l');
-//         }
-//         cy.get('#CodeOutputContent > :nth-child(1)').contains('test');
-//         cy.wait(WAIT_TIME_OUT);
-//     });
+    it('Check print console', () => {
+        cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+          .as('editor');
+        cy.get('@editor').focus();
+        cy.get('@editor').type(codeCheckConsole);
+        if (isMacOSPlatform()) {
+            cy.get('@editor').type('{command}l');
+        } else {
+            cy.get('@editor').type('{ctrl}l');
+        }
+        cy.get('#CodeOutputContent > :nth-child(1)').contains('test');
+        cy.wait(WAIT_TIME_OUT);
+    });
 
-//     it('Check autocompletion', () => {
-//         cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
-//           .as('editor');
-//         cy.get('@editor').focus();
-//         cy.get('@editor').type(codeTestDF);
-//         // make sure have autocompletion dialog
-//         cy.get('@editor').type('{enter}')
-//         cy.get('@editor').type('df.drop');
-//         cy.get('.cm-tooltip-autocomplete').should('be.visible');
-//         cy.get('.cm-read-more-btn').should('be.visible');
-//         cy.get('.cm-read-more-btn').click();
-//         cy.get('#code-doc-content').should('be.visible');
+    it('Check autocompletion', () => {
+        cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+          .as('editor');
+        cy.get('@editor').focus();
+        cy.get('@editor').type(codeTestDF);
+        // make sure have autocompletion dialog
+        cy.get('@editor').type('{enter}')
+        cy.get('@editor').type('df.drop');
+        cy.get('.cm-tooltip-autocomplete').should('be.visible');
+        cy.get('.cm-read-more-btn').should('be.visible');
+        cy.get('.cm-read-more-btn').click();
+        cy.get('#code-doc-content').should('be.visible');
 
-//         // check data
-//         let content = cy
-//             .get('[data-cy="code-editor"] > .cm-editor > .cm-tooltip-autocomplete')
-//             .as('autocomplete-content');
-//         content.contains('drop_duplicates');
+        // check data
+        let content = cy
+            .get('[data-cy="code-editor"] > .cm-editor > .cm-tooltip-autocomplete')
+            .as('autocomplete-content');
+        content.contains('drop_duplicates');
 
-//         // make sure keyboard still work
-//         // editor = cy.get('@editor');
-//         cy.get('@editor').type('{backspace}');
-//         cy.get('@editor').type('{esc}');
-//         cy.get('.cm-tooltip-autocomplete').should('not.exist');
+        // make sure keyboard still work
+        // editor = cy.get('@editor');
+        cy.get('@editor').type('{backspace}');
+        cy.get('@editor').type('{esc}');
+        cy.get('.cm-tooltip-autocomplete').should('not.exist');
 
-//         // make sure have signature tooltip
-//         // editor = cy.get('@editor');
-//         cy.get('@editor').type('p');
-//         cy.get('@editor').type('(');
-//         cy.get('.cm-tooltip-signature').should('be.visible');
+        // make sure have signature tooltip
+        // editor = cy.get('@editor');
+        cy.get('@editor').type('p');
+        cy.get('@editor').type('(');
+        cy.get('.cm-tooltip-signature').should('be.visible');
 
-//         // editor = cy.get('@editor');
-//         cy.get('@editor').type('{esc}');
-//         cy.get('.cm-tooltip-signature').should('not.exist');
+        // editor = cy.get('@editor');
+        cy.get('@editor').type('{esc}');
+        cy.get('.cm-tooltip-signature').should('not.exist');
 
-//         // editor = cy.get('@editor');
-//         removeText(cy.get('@editor'));
-//         cy.get('@editor').type(codeTestDF);
-//         if (isMacOSPlatform()) {
-//             cy.get('@editor').type('{command}l');
-//         } else {
-//             cy.get('@editor').type('{ctrl}l');
-//         }
-//         cy.get('@editor').type('{enter}');
-//         cy.get('@editor').type('df.drop("');
-//         cy.get('.cm-tooltip-autocomplete').should('be.visible');
-//         cy.wait(WAIT_TIME_OUT);
-//     });
+        // editor = cy.get('@editor');
+        removeText(cy.get('@editor'));
+        cy.get('@editor').type(codeTestDF);
+        if (isMacOSPlatform()) {
+            cy.get('@editor').type('{command}l');
+        } else {
+            cy.get('@editor').type('{ctrl}l');
+        }
+        cy.get('@editor').type('{enter}');
+        cy.get('@editor').type('df.drop("');
+        cy.get('.cm-tooltip-autocomplete').should('be.visible');
+        cy.wait(WAIT_TIME_OUT);
+    });
     
-// });
+});
 
-// describe('Test DataFrame', () => {
+describe('Test DataFrame', () => {
 
-//     before(() => {
-//         cy.visit('/');
-//         cy.get('[toolbarname="main.py"]', { timeout: 10000 }).should('be.visible').trigger('click');
-//     })
+    before(() => {
+        cy.visit('/');
+        cy.get('[toolbarname="main.py"]', { timeout: 10000 }).should('be.visible').trigger('click');
+    })
 
-//     beforeEach(() => {
-//         cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content').as('editor');
-//         cy.get('@editor').focus();
-//         removeText(cy.get('@editor'));
-//     })
+    beforeEach(() => {
+        cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content').as('editor');
+        cy.get('@editor').focus();
+        removeText(cy.get('@editor'));
+    })
 
-//     it('Check dataframe', () => {
-//         cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
-//           .as('editor');
-//         cy.get('@editor').focus();
-//         cy.get('@editor').type(codeTestDF);
-//         cy.get('@editor').type('{selectall}');
-//         if (isMacOSPlatform()) {
-//             cy.get('@editor').type('{command}k');
-//             cy.get('@editor').type('{command}l');
-//         } else {
-//             cy.get('@editor').type('{ctrl}k');
-//             cy.get('@editor').type('{ctrl}l');
-//         }
+    it('Check dataframe', () => {
+        cy.get('[data-cy="code-editor"] > .cm-editor > .cm-scroller > .cm-content')
+          .as('editor');
+        cy.get('@editor').focus();
+        cy.get('@editor').type(codeTestDF);
+        cy.get('@editor').type('{selectall}');
+        if (isMacOSPlatform()) {
+            cy.get('@editor').type('{command}k');
+            cy.get('@editor').type('{command}l');
+        } else {
+            cy.get('@editor').type('{ctrl}k');
+            cy.get('@editor').type('{ctrl}l');
+        }
 
-//         cy.get('.MuiTableContainer-root').should('be.visible');
-//         // check columns name
-//         cy.get('.MuiTableHead-root > .MuiTableRow-root > :nth-child(2)').contains('Id');
+        cy.get('.MuiTableContainer-root').should('be.visible');
+        // check columns name
+        cy.get('.MuiTableHead-root > .MuiTableRow-root > :nth-child(2)').contains('Id');
 
-//         cy.get('*[class^="StyledComponents__StyledTableViewHeader"] > :nth-child(2)').click();
-//         cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(1)').contains('Id');
+        cy.get('*[class^="StyledComponents__StyledTableViewHeader"] > :nth-child(2)').click();
+        cy.get('.MuiTableBody-root > :nth-child(1) > :nth-child(1)').contains('Id');
 
-//         cy.window()
-//             .its('store')
-//             .invoke('getState')
-//             .its('dataFrames')
-//             .its('activeDataFrame')
-//             .then((activeDataFrame) => {
-//                 assert.equal(activeDataFrame, "df");
-//             });
+        cy.window()
+            .its('store')
+            .invoke('getState')
+            .its('dataFrames')
+            .its('activeDataFrame')
+            .then((activeDataFrame) => {
+                assert.equal(activeDataFrame, "df");
+            });
 
-//         cy.window()
-//             .its('store')
-//             .invoke('getState')
-//             .its('dataFrames')
-//             .its('tableData')
-//             .then((tableData) => {
-//                 assert.notDeepEqual(tableData, {});
-//             });    
+        cy.window()
+            .its('store')
+            .invoke('getState')
+            .its('dataFrames')
+            .its('tableData')
+            .then((tableData) => {
+                assert.notDeepEqual(tableData, {});
+            });    
 
-//         cy.wait(WAIT_TIME_OUT);
-//     });
+        cy.wait(WAIT_TIME_OUT);
+    });
 
-//     it('Check DF Autocompletion', () => {
-//         let lines = cy
-//             .get('.cm-theme-light > .cm-editor > .cm-scroller > .cm-content')
-//             .as('df-editor')
-//             .children('.cm-line');
-//         expect(lines).to.exist;
-//         lines.its('length').should('be.gt', 0);
+    it('Check DF Autocompletion', () => {
+        let lines = cy
+            .get('.cm-theme-light > .cm-editor > .cm-scroller > .cm-content')
+            .as('df-editor')
+            .children('.cm-line');
+        expect(lines).to.exist;
+        lines.its('length').should('be.gt', 0);
 
-//         let dfEditor = cy.get('@df-editor');
-//         dfEditor.focus();
-//         dfEditor.type('("');
+        let dfEditor = cy.get('@df-editor');
+        dfEditor.focus();
+        dfEditor.type('("');
 
-//         cy.get('.cm-tooltip-autocomplete').should('be.visible');
-//         cy.get('.cm-completionLabel').contains('Alley');
-//         cy.wait(WAIT_TIME_OUT);
-//     })
-// });
+        cy.get('.cm-tooltip-autocomplete').should('be.visible');
+        cy.get('.cm-completionLabel').contains('Alley');
+        cy.wait(WAIT_TIME_OUT);
+    })
+});
 
 describe('Test Rich output result', () => {
 
