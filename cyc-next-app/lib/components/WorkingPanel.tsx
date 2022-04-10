d93b4fe24704e743110cb594584dfc1150af6b23
Bach Bui <bui.duybach@gmail.com>
2022-04-10 14:46:34 -0700

    cosmetic changes

--- cyc-next-app/lib/components/WorkingPanel.tsx
+++ cyc-next-app/lib/components/WorkingPanel.tsx
@@ -1,5 +1,8 @@
 import React, { useMemo, useState } from "react";
-import { WorkingPanel as StyledWorkingPanel, WorkingPanelSplitPanel } from "./StyledComponents";
+import {
+    WorkingPanel as StyledWorkingPanel,
+    WorkingPanelSplitPanel,
+} from "./StyledComponents";
 import CodePanel from "./code-panel/CodePanel";
 import RichOutputPanel from "./richoutput-panel/RichOutputPanel";
 import DFManager from "./dataframe-manager/DataFrameManager";
@@ -8,13 +11,14 @@ import FileExplorer from "./file-manager/FileExplorer";
 import { useSelector } from "react-redux";
 import Pane from "react-split-pane-v2/lib/Pane";
 import { ViewMode } from "../interfaces/IApp";
-import store, { RootState } from '../../redux/store';
-// import { extensions, getCodeText, onCodeMirrorChange } from "./code-panel/CodeMirror";
+import { RootState } from "../../redux/store";
 
-const WorkingPanel = () => {  
-	const showProjectExplore = useSelector((state: RootState) => state.projectManager.showProjectExplore);
-	const [viewMode, setViewMode] = useState(ViewMode.HORIZONTAL)
-	return (
+const WorkingPanel = () => {
+    const showProjectExplore = useSelector(
+        (state: RootState) => state.projectManager.showProjectExplore
+    );
+    const [viewMode, setViewMode] = useState<ViewMode>(ViewMode.VERTICAL);
+    return (
         <StyledWorkingPanel>
             {/* have to do complicated inline style because of this 
 			https://newbedev.com/absolute-positioning-ignoring-padding-of-parent */}
