643a3923fadd7c79ae775087deae747d104ad677
Vick Nguyen <vicknguyen1992@gmail.com>
2022-05-06 14:57:36 +0700

    Add kernel control test cases

--- cyc_next_app/lib/components/tests/CypressIds.js
+++ cyc_next_app/lib/components/tests/CypressIds.js
@@ -1,12 +1,12 @@
 // store all ids for automate testing
 const CypressIds = {
-    codeEditor: 'code-editor',
-    dfEditor: 'df-editor',
-    projectRoot: 'project-root',
-    newFileItem: 'new-file-item',
-    dfStatsCheckboxAll: 'df-stats-checkbox',
-    dfViewMode: 'df-viewmode',
-    dfSummaryTable: 'df-summary-table',
+    codeEditor: "code-editor",
+    dfEditor: "df-editor",
+    projectRoot: "project-root",
+    newFileItem: "new-file-item",
+    dfStatsCheckboxAll: "df-stats-checkbox",
+    dfViewMode: "df-viewmode",
+    dfSummaryTable: "df-summary-table",
 };
 
 export default CypressIds;
