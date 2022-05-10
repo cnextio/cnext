8e388f72dc1c8400e13518c418c571a874ea9dd3
Vick Nguyen <vicknguyen1992@gmail.com>
2022-05-10 11:47:00 +0700

    implement sentry with nextjs

--- cyc_next_app/next.config.js
+++ cyc_next_app/next.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const sentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+    dsn: "https://ae2ea08ad87b4d8e99ddd744438a2739@o1238555.ingest.sentry.io/6389364",
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+moduleExports = {
     reactStrictMode: true,
     typescript: {
         // !! WARN !!
@@ -9,3 +28,5 @@ module.exports = {
         ignoreBuildErrors: true,
     },
 };
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
