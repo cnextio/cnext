145f7d4a706acde97fd4625e5cef9fc2fce7c2b6
HoaTV <thaivanhoa.dtmtptit@gmail.com>
2022-05-20 11:39:30 +0700

    update sentry config

--- cyc_next_app/next.config.js
+++ cyc_next_app/next.config.js
@@ -1,24 +1,5 @@
 /** @type {import('next').NextConfig} */
-const { withSentryConfig } = require("@sentry/nextjs");
-
-const sentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
-    dsn: "https://ae2ea08ad87b4d8e99ddd744438a2739@o1238555.ingest.sentry.io/6389364",
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.1,
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-moduleExports = {
+module.exports = {
     reactStrictMode: true,
     typescript: {
         // !! WARN !!
@@ -28,5 +9,3 @@ moduleExports = {
         ignoreBuildErrors: true,
     },
 };
-
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
