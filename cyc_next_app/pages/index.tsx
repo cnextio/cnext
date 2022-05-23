368dd07a7b4124808505f824cbe5ba6148947abd
Bach Bui <bui.duybach@gmail.com>
2022-05-23 13:53:37 -0700

    add missing files to support account management

--- cyc_next_app/pages/index.tsx
+++ cyc_next_app/pages/index.tsx
@@ -1,12 +1,15 @@
 import { createTheme, StyledEngineProvider, ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import type { NextPage } from 'next';
-import React from "react";
+import React, { Fragment } from "react";
 import Helmet from 'react-helmet';
 import { Provider } from 'react-redux';
+import { Auth0Provider } from "@auth0/auth0-react";
 import store from '../redux/store/index';
 import Main from '../lib/components/Main';
 import ErrorBoundary from "../lib/components/error-boundary/ErrorBoundary";
 import { createGlobalStyle, ThemeProvider as StyledThemeProvider } from "styled-components";
+import LoginButton from '../lib/components/user-manager/Login';
+import LogoutButton from '../lib/components/user-manager/Logout';
 const GlobalStyle = createGlobalStyle`
   body {
     height: 100vh;
@@ -17,24 +20,34 @@ const theme = createTheme({});
 
 const Home: NextPage = () => {
     return (
-        <Provider store={store}>
-            <React.Fragment>
-                <GlobalStyle />
-                <Helmet
-                    titleTemplate='%s | CNext'
-                    defaultTitle='CNext - A Data-Centric Platform for DS and AI Workflows'
-                />
-                <StyledEngineProvider injectFirst>
-                    <StyledThemeProvider theme={theme}>
-                        <MuiThemeProvider theme={theme}>
-                            <ErrorBoundary>
-                                <Main />
-                            </ErrorBoundary>
-                        </MuiThemeProvider>
-                    </StyledThemeProvider>
-                </StyledEngineProvider>
-            </React.Fragment>
-        </Provider>
+        <Fragment>
+            <Auth0Provider
+                domain="dev-r6dezw30.us.auth0.com"
+                clientId="MoyhDEIlW3YI87mLieDmgodcFOl0htLQ"
+                redirectUri={"http://localhost:3000"}
+            >
+                {/* <LoginButton />
+                <LogoutButton /> */}
+                <Provider store={store}>
+                    <React.Fragment>
+                        <GlobalStyle />
+                        <Helmet
+                            titleTemplate="%s | CNext"
+                            defaultTitle="CNext - A Data-Centric Platform for DS and AI Workflows"
+                        />
+                        <StyledEngineProvider injectFirst>
+                            <StyledThemeProvider theme={theme}>
+                                <MuiThemeProvider theme={theme}>
+                                    <ErrorBoundary>
+                                        <Main />
+                                    </ErrorBoundary>
+                                </MuiThemeProvider>
+                            </StyledThemeProvider>
+                        </StyledEngineProvider>
+                    </React.Fragment>
+                </Provider>
+            </Auth0Provider>
+        </Fragment>
     );
 };
 
