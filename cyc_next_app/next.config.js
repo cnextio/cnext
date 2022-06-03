1694c32a03645ee12e528f5906369bfcd9e07959
nguyen viet <vietnguyen11101992@gmail.com>
2022-06-03 09:12:45 +0000

    update sentry on nextJS

--- cyc_next_app/next.config.js
+++ cyc_next_app/next.config.js
@@ -1,11 +1,33 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-    reactStrictMode: true,
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true,
-    },
-};
+ const { withSentryConfig } = require("@sentry/nextjs");
+ 
+ const sentryWebpackPluginOptions = {
+     // Additional config options for the Sentry Webpack plugin. Keep in mind that
+     // the following options are set automatically, and overriding them is not
+     // recommended:
+     //   release, url, org, project, authToken, configFile, stripPrefix,
+     //   urlPrefix, include, ignore
+    dsn: "https://ae2ea08ad87b4d8e99ddd744438a2739@o1238555.ingest.sentry.io/6389364",
+    dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
+ 
+     // Set tracesSampleRate to 1.0 to capture 100%
+     // of transactions for performance monitoring.
+     // We recommend adjusting this value in production
+     tracesSampleRate: 0.1,
+     silent: true, // Suppresses all logs
+     // For all available options, see:
+     // https://github.com/getsentry/sentry-webpack-plugin#options.
+ };
+ 
+ moduleExports = {
+     reactStrictMode: true,
+     typescript: {
+         // !! WARN !!
+         // Dangerously allow production builds to successfully complete even if
+         // your project has type errors.
+         // !! WARN !!
+         ignoreBuildErrors: true,
+     },
+ };
+ 
+ module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
