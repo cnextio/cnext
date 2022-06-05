4bd8d06858ee37975de79fb87066fee1237af836
nguyen viet <vietnguyen11101992@gmail.com>
2022-06-05 06:19:46 +0000

    update sentry client

--- cyc_next_app/sentry.client.config.js
+++ cyc_next_app/sentry.client.config.js
@@ -1,7 +1,7 @@
 const Sentry = require("@sentry/nextjs");
 
 Sentry.init({
-    dsn: "https://ae2ea08ad87b4d8e99ddd744438a2739@o1238555.ingest.sentry.io/6389364",
+    dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
     tracesSampleRate: 0.1,
