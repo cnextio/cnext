8004876b3114fe276ad188803ae2d6b1aba1a194
Vick Nguyen <vicknguyen1992@gmail.com>
2022-06-05 14:01:09 +0700

    update sentry

--- cyc_next_app/next.config.js
+++ cyc_next_app/next.config.js
@@ -8,6 +8,7 @@ const sentryWebpackPluginOptions = {
     //   release, url, org, project, authToken, configFile, stripPrefix,
     //   urlPrefix, include, ignore
     dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
+    authToken: "19f6f8e90778487cb1c2333518a47def664d5734769b40ac857e4bede4222f2d",
     tracesSampleRate: 0.1,
     silent: true, // Suppresses all logs
     // For all available options, see:
--- cyc_next_app/sentry.server.config.js
+++ cyc_next_app/sentry.server.config.js
@@ -2,6 +2,7 @@ const Sentry = require("@sentry/nextjs");
 
 Sentry.init({
     dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
+    authToken: "19f6f8e90778487cb1c2333518a47def664d5734769b40ac857e4bede4222f2d",
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
