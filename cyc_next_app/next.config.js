ca5f71f0ba4aeb50b627832bc38e33c2ae097353
nguyen viet <vietnguyen11101992@gmail.com>
2022-06-05 06:33:05 +0000

    update nextjs config

--- cyc_next_app/next.config.js
+++ cyc_next_app/next.config.js
@@ -9,9 +9,6 @@ const sentryWebpackPluginOptions = {
     //   urlPrefix, include, ignore
     dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
     authToken: "19f6f8e90778487cb1c2333518a47def664d5734769b40ac857e4bede4222f2d",
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
     tracesSampleRate: 0.1,
     silent: true, // Suppresses all logs
     // For all available options, see:
