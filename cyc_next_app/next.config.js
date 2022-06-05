8821debc3184708d76f51261e2186b4ce3b46e7f
nguyen viet <vietnguyen11101992@gmail.com>
2022-06-05 06:07:10 +0000

    update nextjs sentry

--- cyc_next_app/next.config.js
+++ cyc_next_app/next.config.js
@@ -1,38 +1,24 @@
-// /** @type {import('next').NextConfig} */
-//  const { withSentryConfig } = require("@sentry/nextjs");
- 
-//  const sentryWebpackPluginOptions = {
-//      // Additional config options for the Sentry Webpack plugin. Keep in mind that
-//      // the following options are set automatically, and overriding them is not
-//      // recommended:
-//      //   release, url, org, project, authToken, configFile, stripPrefix,
-//      //   urlPrefix, include, ignore
-//     dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
- 
-//      // Set tracesSampleRate to 1.0 to capture 100%
-//      // of transactions for performance monitoring.
-//      // We recommend adjusting this value in production
-//      tracesSampleRate: 0.1,
-//      silent: true, // Suppresses all logs
-//      // For all available options, see:
-//      // https://github.com/getsentry/sentry-webpack-plugin#options.
-//  };
- 
-//  moduleExports = {
-//      reactStrictMode: true,
-//      typescript: {
-//          // !! WARN !!
-//          // Dangerously allow production builds to successfully complete even if
-//          // your project has type errors.
-//          // !! WARN !!
-//          ignoreBuildErrors: true,
-//      },
-//  };
- 
-//  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
-
 /** @type {import('next').NextConfig} */
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const sentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+    dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
+    authToken: "19f6f8e90778487cb1c2333518a47def664d5734769b40ac857e4bede4222f2d",
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.1,
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+moduleExports = {
     reactStrictMode: true,
     typescript: {
         // !! WARN !!
@@ -42,3 +28,5 @@ module.exports = {
         ignoreBuildErrors: true,
     },
 };
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
