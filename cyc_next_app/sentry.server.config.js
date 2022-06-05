a1d3125bee134a9355ed8d305cd5fa6286854b73
nguyen viet <vietnguyen11101992@gmail.com>
2022-06-05 06:20:15 +0000

    update sentry server

--- cyc_next_app/sentry.server.config.js
+++ cyc_next_app/sentry.server.config.js
@@ -1,7 +1,7 @@
 const Sentry = require("@sentry/nextjs");
 
 Sentry.init({
-    dsn: "https://ae2ea08ad87b4d8e99ddd744438a2739@o1238555.ingest.sentry.io/6389364",
+    dsn: "https://25a5df08f7ea47be8a9922441cba00d1@o1259763.ingest.sentry.io/6435278",
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
