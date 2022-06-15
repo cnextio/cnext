77bc3ced424378f88a8af680b0ccc03d27c7bfc7
HoaTV <thaivanhoa.dtmtptit@gmail.com>
2022-06-15 10:36:16 +0700

    update bug

--- cyc_next_app/__main__.py
+++ cyc_next_app/__main__.py
@@ -28,20 +28,32 @@ def change_path(path):
     my_file.close()
 
     project_id = str(uuid.uuid1())
-    string_list[0] = 'active_project: ' + '"' + project_id + '"' + '\n'
-    content_2 = string_list[2];
-    string_list[2] = content_2[:FIELD_LENGTH] + 'id: ' + '"' + project_id + '"' + '\n'
+    # handler case empty when use teminal
+    if not string_list:
+        # build_all_content
+        my_file = open(FILE_NAME, 'w')
+        line1 ='active_project: "4b9a3cfd-ec55-11ec-b7b8-309c238898ae"\n'
+        line2 = 'open_projects:\n'
+        line3 = '\t- id: "4b9a3cfd-ec55-11ec-b7b8-309c238898ae"\n'
+        line4 = '\tname: "Skywalker"\n'
+        line5 = '\tpath: ' + path
+        my_file.writelines([line1, line2, line3,line4,line5])
+        my_file.close()
+    else:
+        string_list[0] = 'active_project: ' + '"' + project_id + '"' + '\n'
+        content_2 = string_list[2];
+        string_list[2] = content_2[:FIELD_LENGTH] + 'id: ' + '"' + project_id + '"' + '\n'
 
-    content = string_list[CHANGE_LINE_NUMBER]
-    string_list[CHANGE_LINE_NUMBER] = content[:FIELD_LENGTH] + \
-        'path: ' + '\'' + path + '\'' + '\n'
-    print(string_list[CHANGE_LINE_NUMBER].lstrip().strip())
-    my_file = open(FILE_NAME, 'w')
-    new_file_contents = ''.join(string_list)
-    # Convert `string_list` to a single string
-    my_file.write(new_file_contents)
-    my_file.close()
-    print('Map path for sample project done!')
+        content = string_list[CHANGE_LINE_NUMBER]
+        string_list[CHANGE_LINE_NUMBER] = content[:FIELD_LENGTH] + \
+            'path: ' + '\'' + path + '\'' + '\n'
+        print(string_list[CHANGE_LINE_NUMBER].lstrip().strip())
+        my_file = open(FILE_NAME, 'w')
+        new_file_contents = ''.join(string_list)
+        # Convert `string_list` to a single string
+        my_file.write(new_file_contents)
+        my_file.close()
+        print('Map path for sample project done!')
 
 
 def ask():
