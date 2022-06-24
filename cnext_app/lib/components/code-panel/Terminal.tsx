b9d7be6404bcf8a79a2711973fae3a12f1a7297e
tuancon <tuankt@lattehub.com>
2022-06-24 22:21:06 +0700

    change color

--- cnext_app/lib/components/code-panel/Terminal.tsx
+++ cnext_app/lib/components/code-panel/Terminal.tsx
@@ -16,30 +16,37 @@ const Term = () => {
     const [cursorPosition, setCursorPosition] = useState<number>(0);
     const [history, setHistory] = useState<{ [key: string]: string }>();
 
-    const [pathPrefix, setPathPrefix] = useState<string>(`~$ `);
+    const [pathPrefix, setPathPrefix] = useState<string>(``);
     const [isMountTerm, setIsMountTerm] = useState<boolean>(false);
     const fitAddon = new FitAddon();
     const searchAddon = new SearchAddon();
     // Connect Socket
+    const updatePath = (data) => {
+        setPathPrefix(data);
+    };
     const socketInit = () => {
-        console.log("test");
-
         socket.emit("ping", "Terminal");
         socket.on("res-data", (data) => {
-            console.log("data loi", data);
+            console.log("ResponseTerm", data);
 
             if (xtermRef?.current?.terminal) {
                 const term = xtermRef?.current?.terminal;
-                if (data?.code ===1) {
-                    term.write(
-                        c.red(
-                            `${data.cmd}: The term ${data.cmd} is not recognized as the name of  a cmdlet, function, \r\n script file, or operable program`
-                        )
-                    );
-                    term.write("\r\n" + pathPrefix);
+                if (data?.type === `error`) {
+                    term.write("\r\n");
+                    term.write(c.red(`${data.name}${data.message}`));
+                    // term.write("\r\n" + pathPrefix);
+                }
+                if (data?.type === `path`) {
+                    console.log("change path");
+                    // setPathPrefix(data => );
+                    term.write(pathPrefix);
+                    // updatePath(`${data.path}> `)
+                    // setPathPrefix(`${data.path}> `);
                 } else {
                     term.write(data);
-                    term.write("\r\n" + pathPrefix);
+                    console.log("pathPrefix", pathPrefix);
+
+                    // term.write("\r\n" + pathPrefix);
                 }
             }
         });
@@ -76,7 +83,9 @@ const Term = () => {
             socket.off(WebAppEndpoint.Terminal);
         };
     }, []); //TODO: run this only once - not on rerender
-
+    useEffect(() => {
+        console.log(pathPrefix);
+    }, [pathPrefix]); //
     function onTermData(data: any) {
         setIsMountTerm(true);
 
@@ -111,9 +120,8 @@ const Term = () => {
                     break;
 
                 case "[A": // Up
-                    term.write(history ? history[`0`] : "");
+                    term.write(`${Math.random()}`);
                     setInput(history ? history[`0`] : "");
-
                     break;
 
                 case "[B": //  Bottom arrow
@@ -125,14 +133,12 @@ const Term = () => {
         }
         if (code === KeyCode.Enter && input.length > 0) {
             if (input === "cls" || input === "clear") {
-                term.clear();
-                // term.write("\r\n" + pathPrefix);
-            } else {
-                term.write("\r\n" + pathPrefix);
+                term.write("\x1bc");
             }
-            // sendMessage({ content: input });
+
             socket.emit(WebAppEndpoint.Terminal, JSON.stringify({ content: input }));
-            console.log("WebAppEndpoint.Terminal", WebAppEndpoint.Terminal);
+            term.write("\r\n");
+            // sendMessage({ content: input });
 
             setHistory({
                 ...history,
@@ -152,22 +158,7 @@ const Term = () => {
             return;
         } else {
             term.write(data);
-            // Add general key press characters to the terminal
-            // if (cursorPosition === 0) {
-            //   term.write(data);
-            // } else {
-            //   console.log("input", input);
-            //   console.log("data", data);
-            //   console.log("cursorPosition", cursorPosition);
-            //   console.log("tesst", test);
-            //   console.log(
-            //     "tesst texxt",
-            //     input.substr(0, test) + data + input.substr(test)
-            //   );
-            //   term.write(input.substr(0, test) + data + input.substr(test));
-            //   // term.write(`\u001b[0;5H`);
-            // }
-            // setCursor((input + data).length);
+
             setInput(input + data);
         }
     }
@@ -181,31 +172,29 @@ const Term = () => {
             xtermRef.current.terminal.write("\b \b");
             setInput(input.slice(0, -1));
         }
-        console.log(data.domEvent.keyCode, "key");
     };
     useEffect(() => {
-        var init_width = 9;
-        var init_height = 17;
-        var windows_width = window.innerWidth;
-        var windows_height = window.innerHeight;
-        const cols = Math.floor(windows_width / init_width);
-        const rows = Math.floor(windows_height / init_height) - 2;
-
         if (xtermRef?.current?.terminal && !isMountTerm) {
-            xtermRef.current.terminal.write(pathPrefix);
+            setIsMountTerm(true);
+            socket.emit(WebAppEndpoint.Terminal, JSON.stringify({ content: input, type: "path" }));
+
+            // xtermRef.current.terminal.write(pathPrefix);
         }
     }, [xtermRef.current]);
+    const onResize = () => {
+        if (xtermRef?.current?.terminal) {
+            fitAddon.fit();
+        }
+    };
     return (
         <>
+            {pathPrefix}
             <XTerm
-                onResize={() => {
-                    console.log("ádfdsf", xtermRef.current.terminal),
-                        xtermRef.current.terminal.fit();
-                }}
+                onResize={onResize}
                 options={{
                     fontSize: 16,
                     fontWeight: 900,
-                    theme: { background: "black", foreground: "white" },
+                    theme: { background: "white", foreground: "#00294d" },
                 }}
                 onKey={onTermKey}
                 onData={onTermData}
