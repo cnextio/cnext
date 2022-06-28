606b0b0a00edeb9917966a532873b7361093c1ef
Bach Bui <bui.duybach@gmail.com>
2022-06-28 12:26:53 -0700

    1. remove group folding for now because of a bug 2. change file display filter

--- cnext_app/lib/components/code-panel/CodeEditor.tsx
+++ cnext_app/lib/components/code-panel/CodeEditor.tsx
@@ -141,7 +141,7 @@ const CodeEditor = () => {
 
     const defaultExtensions = [
         basicSetup,
-        foldService.of(getGroupFoldRange),
+        // foldService.of(getGroupFoldRange),
         lineNumbers(),
         editStatusGutter(store.getState().projectManager.inViewID, getCodeLine(store.getState())),
         groupWidget(),
