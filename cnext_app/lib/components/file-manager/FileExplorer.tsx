97c64bf4826cd6ad5ebf21eae1f09813e8d8f43d
Bach Bui <bui.duybach@gmail.com>
2022-07-03 10:58:07 -0700

    improve file explorer style

--- cnext_app/lib/components/file-manager/FileExplorer.tsx
+++ cnext_app/lib/components/file-manager/FileExplorer.tsx
@@ -11,6 +11,7 @@ import {
     OpenProjectTree,
     FileItemLabel,
     ProjectExplorerContainer,
+    OpenProjectItem,
 } from "../StyledComponents";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
@@ -470,7 +471,7 @@ const FileExplorer = (props: any) => {
                     onNodeToggle={handleDirToggle}
                 >
                     {activeProject != null && (
-                        <FileItem
+                        <OpenProjectItem
                             nodeId={relativeProjectPath}
                             data-cy={CypressIds.projectRoot}
                             label={
@@ -490,7 +491,7 @@ const FileExplorer = (props: any) => {
                             }}
                         >
                             {renderFileItems(projectItem?.path, relativeProjectPath)}
-                        </FileItem>
+                        </OpenProjectItem>
                     )}
                 </OpenProjectTree>
             );
