85f472817a7531bdb70cad6a1d0e19fa1d616377
Huy TQ <5723282+imhuytq@users.noreply.github.com>
2022-07-17 23:12:03 +0000

    fix build + ci

--- express/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-
-app.use(express.static('../cnext_app/out'))
-
-app.listen(3000, () => {
-    console.log('ok');
-});
