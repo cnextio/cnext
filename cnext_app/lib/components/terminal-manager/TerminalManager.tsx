135468fa7c149462c7ea388a907d6cb0b5459fd1
tuanka <mrkathust@gmail.com>
2022-07-28 11:04:47 +0700

    check cookie

--- cnext_app/lib/components/terminal-manager/TerminalManager.tsx
+++ cnext_app/lib/components/terminal-manager/TerminalManager.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setConfigTerminal } from "../../../redux/reducers/TerminalRedux";
+import { getCookie } from "../../../utils";
 import { CommandName, WebAppEndpoint } from "../../interfaces/IApp";
 import socket from "../Socket";
 
+const cookieNameJupyterServer = `_xsrf`;
 const ConfigTerminal = "ConfigTerminal";
 
 const TerminalManager = () => {
@@ -28,9 +30,20 @@ const TerminalManager = () => {
         socket.on(WebAppEndpoint.Terminal, (result: string) => {
             try {
                 if (JSON.parse(result).command_name === CommandName.get_jupyter_server_config) {
-                    dispatch(setConfigTerminal(JSON.parse(result).content));
+                    const config = JSON.parse(result).content;
+                    dispatch(setConfigTerminal(config));
+
+                    if (!getCookie(cookieNameJupyterServer)) {
+                        const BASEURL = `http://localhost:${config.port}?token=${config.token}`;
+                        const cmd = window.open(`${BASEURL}`);
+                        setTimeout(() => {
+                            cmd?.close();
+                        }, 0);
+                    }
                 }
             } catch (error) {
+                console.log(`error`, error);
+
                 throw error;
             }
         });
