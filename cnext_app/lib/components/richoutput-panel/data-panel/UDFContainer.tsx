0eb11a0eba6ec73ba0c302ae07515eded4950da8
tuanka <mrkathust@gmail.com>
2022-11-01 23:40:15 +0700

    add zoom img header

--- cnext_app/lib/components/richoutput-panel/data-panel/UDFContainer.tsx
+++ cnext_app/lib/components/richoutput-panel/data-panel/UDFContainer.tsx
@@ -6,10 +6,21 @@ import { useSelector } from "react-redux";
 import store, { RootState } from "../../../../redux/store";
 import { createPlot } from "../../dataframe-manager/udf/libUDF";
 import { UDFOutputType } from "../../../interfaces/IDataFrameManager";
-
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
 export function UDFContainer({ udfName, df_id, col_name, width, height }) {
     const udfData = useSelector((state: RootState) => getUDFData(state));
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
 
+    const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
     function getUDFData(state: RootState) {
         let dfMetadata = state.dataFrames.metadata[df_id];
 
@@ -26,7 +37,40 @@ export function UDFContainer({ udfName, df_id, col_name, width, height }) {
         let registeredUDFs = store.getState().dataFrames.registeredUDFs;
         if (udfData && registeredUDFs) {
             if (registeredUDFs.udfs[udfName].config.type === UDFOutputType.IMAGE) {
-                return <SmallVizContainer>{createPlot(udfData, width, height)}</SmallVizContainer>;
+                return (
+                    <div>
+                        <SmallVizContainer
+                            aria-owns={open ? "zoom-viz" : undefined}
+                            aria-haspopup="true"
+                            onMouseEnter={handlePopoverOpen}
+                            onMouseLeave={handlePopoverClose}
+                        >
+                            {createPlot(udfData, width, height)}
+                        </SmallVizContainer>
+                        <Popover
+                            id="zoom-viz"
+                            sx={{
+                                pointerEvents: "none",
+                            }}
+                            open={open}
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            onClose={handlePopoverClose}
+                            disableRestoreFocus
+                        >
+                            <SmallVizContainer>
+                                {createPlot(udfData, width * 4, height * 4)}
+                            </SmallVizContainer>
+                        </Popover>
+                    </div>
+                );
             } else {
                 // return <SmallVizContainer>{udfData['text/plain']}</SmallVizContainer>;
                 return null;
