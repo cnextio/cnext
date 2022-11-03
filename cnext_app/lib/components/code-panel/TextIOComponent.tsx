6487a70cc84dacc84866b604f17ea98f2967866a
Bach Bui <bui.duybach@gmail.com>
2022-11-03 12:51:28 -0700

    refactor: change padding

--- cnext_app/lib/components/code-panel/TextIOComponent.tsx
+++ cnext_app/lib/components/code-panel/TextIOComponent.tsx
@@ -20,7 +20,7 @@ const TextIOComponent = React.memo(() => {
     return (
         <TextIOContainer>
             {console.log("Render TextIOContainer")}
-            <TextIOHeader>
+            <TextIOHeader style={{ paddingBottom: "5px" }}>
                 {[TAB.CONSOLE, TAB.TERMINAL].map((item, key) => {
                     return (
                         <TextIOHeaderText
