e8f521f849d0a2e930947644c388412e90c60515
Bach Bui <bui.duybach@gmail.com>
2022-11-18 06:38:23 -0800

    feat: [wip] add supports to implement jupyter widget

--- cnext_server/server/python/user_space/ipython/kernel.py
+++ cnext_server/server/python/user_space/ipython/kernel.py
@@ -60,9 +60,9 @@ class IPythonKernel():
                 log.info('Kernel execution lock released')
                 self._set_execution_complete_condition(True)
             if self.km.is_alive():
-                return True
+                return {"success": True, "kernel_info": self._get_kernel_info()}
             else:
-                return False
+                {"success": False}
         except:
             trace = traceback.format_exc()
             log.info("Exception %s" % (trace))
@@ -76,9 +76,9 @@ class IPythonKernel():
                 self.km.shutdown_kernel(now=True)
                 log.info('Kernel shutdown')
                 if not self.km.is_alive():
-                    return True
+                    {"success": True}
                 else:
-                    return False
+                    {"success": False}
         except:
             trace = traceback.format_exc()
             log.info("Exception %s" % (trace))
@@ -103,24 +103,30 @@ class IPythonKernel():
                 self._set_execution_complete_condition(True)
 
             if self.km.is_alive():
-                return True
+                return {"success": True, "kernel_info": self._get_kernel_info()}
             else:
-                return False
+                return {"success": False}
         except:
             trace = traceback.format_exc()
             log.info("Exception %s" % (trace))
-        return False
+        return {"success": False}
 
     def interrupt_kernel(self):
         try:
             if self.km.is_alive():
                 self.km.interrupt_kernel()
                 log.info('Kerel interrupted')
-            return True
+            return {"success": True}
         except:
             trace = traceback.format_exc()
             log.info("Exception %s" % (trace))
-        return False
+        return {"success": False}
+    
+    def _get_kernel_info(self):
+        return {'id': self.km.kernel_id, 'name': self.km.kernel_name, 'spec': self.km.kernel_spec, 'connection_info': self.kc.get_connection_info()}
+
+    def get_kernel_info(self):
+        return {"success": True, "kernel_info": self._get_kernel_info()}
 
     def is_alive(self):
         return self.kc.is_alive() if self.kc else False
@@ -196,6 +202,7 @@ class IPythonKernel():
                     if self.execute_lock.locked():
                         self.execute_lock.release()
                         log.info('Kernel execution lock released')
+                    break
             log.info('Stop stream %s' % stream_type)
         except:
             trace = traceback.format_exc()
@@ -220,6 +227,16 @@ class IPythonKernel():
             if self.kc:
                 log.info('Send stdin input to kernel ')
                 self.kc.input(input_text)
+        except:
+            trace = traceback.format_exc()
+            log.info("Exception %s" % (trace))
+
+    def send_comm_msg(self, message):
+        try:
+            if self.kc:
+                log.info('Send comm_msg to kernel ')
+                msg = self.kc.session.msg("comm_msg", message)
+                self.kc.shell_channel.send(msg)
         except:
             trace = traceback.format_exc()
             log.info("Exception %s" % (trace))
